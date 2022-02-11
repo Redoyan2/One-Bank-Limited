306e762ed38f3d60819dfc26b953707bc51d5872
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-02-12 04:29:25 +0600

    complete balance

--- Javascripts/banking.js
+++ Javascripts/banking.js
@@ -1,7 +1,26 @@
+
 const dipositAmount = document.getElementById('diposit-amount');
 const dipositInput = document.getElementById('diposit-input');
 const dipositBtn = document.getElementById('diposit-btn');
-dipositBtn.addEventListener('click', function(){
-    dipositAmount.innerText = dipositInput.value;
+const widowAmount = document.getElementById('widow-amount');
+const widowInput = document.getElementById('widow-input');
+const widowBtn = document.getElementById('widow-btn');
+const balanceAmount = document.getElementById('balance-amount');
+
+
+
+
+dipositBtn.addEventListener('click', function () {
+    dipositAmount.innerText = parseFloat(dipositInput.value) + parseFloat(dipositAmount.innerText);
+    balanceAmount.innerText = parseFloat(balanceAmount.innerText) + parseFloat(dipositInput.value);
     dipositInput.value = '';
 })
+
+
+
+widowBtn.addEventListener('click', function () {
+    widowAmount.innerText = parseFloat(widowInput.value) + parseFloat(widowAmount.innerText);
+    balanceAmount.innerText = parseFloat(balanceAmount.innerText) - parseFloat(widowInput.value);
+    widowInput.value = '';
+})
+
